3c3ec902b87976decd1e568a65f3eac5f6799373
PiAir <PiAir@users.noreply.github.com>
2020-05-15 08:23:14 +0200

    Added functionality
    Selection of activities (only walks in my case)
    Added loop to get full polyline for activity
    Added random color to each polyline
    Calculate boundary of combined polylines and set map  to that boundary
    Added (rude) error checking for walks that don't have polyline

--- Strava_Api/LeafletUpdates/strava_api.js
+++ Strava_Api/LeafletUpdates/strava_api.js
@@ -3,38 +3,64 @@ const auth_link = "https://www.strava.com/oauth/token"
 
 function getActivites(res){
 
-    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    // console.log(res)
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?per_page=60&access_token=${res.access_token}`
     fetch(activities_link)
         .then((res) => res.json())
         .then(function (data){
-
-            var map = L.map('map').setView([38.895417, -77.033616], 11);
+        
+            var map = L.map('map').setView([51.45668031498194, 5.774687314453169], 11);
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
+            var myHeaders = new Headers();
+            myHeaders.append("Authorization", "Bearer " + res.access_token);
+
+            
+            var requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                redirect: 'follow'
+            };
+            
+            var latlngbounds = new L.latLngBounds(); // contains boundaries of the lines
+            
             for(var x=0; x<data.length; x++){
+                console.log("X = " + x + " ID = " + data[x].id);
+                // only map walks
+                if (data[x].type == "Walk") {
+                    console.log("Walking")
+                    fetch("https://www.strava.com/api/v3/activities/"+data[x].id+"?include_all_efforts=true", requestOptions)
+                        .then((res) => res.json())
+                        .then(function (data){
+                            // some walks don't have gps info because they were added manually
+                            try {
+                                var polyline = L.Polyline.fromEncoded(data.map.polyline);
+                                latlngbounds.extend(polyline.getBounds()); // store boundary info
+                                var coordinates = L.Polyline.fromEncoded(data.map.polyline).getLatLngs();
+                                L.polyline(
+                                    coordinates,
+                                    {
+                                        color: "#" + Math.floor(Math.random()*16777215).toString(16),
+                                        weight:5,
+                                        opacity:.7,
+                                        lineJoin:'round'
+                                    }
+                                ).addTo(map)
+                                map.fitBounds(latlngbounds); // set boundary
+                            }  catch(err) {
+                                console.log("Cannot read map for activity " + x + " = " + data.name + " ID = " + data.id);
+                            }
+                        })
+                        .catch(error => console.log('error', error));
 
-                console.log(data[x].map.summary_polyline)
-                var coordinates = L.Polyline.fromEncoded(data[x].map.summary_polyline).getLatLngs()
-                console.log(coordinates)
+                }
 
-                L.polyline(
-
-                    coordinates,
-                    {
-                        color:"green",
-                        weight:5,
-                        opacity:.7,
-                        lineJoin:'round'
-                    }
-
-                ).addTo(map)
             }
-
-        }
-        )
+          
+        })
 }
 
     
@@ -48,9 +74,9 @@ function reAuthorize() {
         },
         body: JSON.stringify({
 
-            client_id: 'xxxxx',
-            client_secret: 'xxxxx',
-            refresh_token: 'xxxxx',
+            client_id: 'xxxx',
+            client_secret: 'xxxxxxx',
+            refresh_token: 'xxxxxxx',
             grant_type: 'refresh_token'
 
         })
